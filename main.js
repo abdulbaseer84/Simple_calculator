2347ca35015dc4426283b0d45edcf9b5f472a85d
Abdul Baseer <abdulbaseer848@gmail.com>
2023-05-19 00:14:41 +0530

    Simple_Calculator

---
+++ main.js
@@ -0,0 +1,8 @@
+const display11=document.querySelector('.display1');
+const display22=document.querySelector('.display2');
+const numbersA1=document.querySelector('.number');
+const operationB1=document.querySelector('.operation');
+const equalE1=document.querySelector('.equal');
+const clearE1=document.querySelector('allclear');
+const clearlastE1=document.querySelector('lastentity_clear');
+

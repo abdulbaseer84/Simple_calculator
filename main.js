a546c51206d5635a115c591be998f86b451fef46
Abdul Baseer <abdulbaseer848@gmail.com>
2023-05-20 12:03:24 +0530

    Simple_Calculator

--- main.js
+++ main.js
@@ -77,3 +77,48 @@ clearLastE1.addEventListener('click',(e)=>{
     display2E1.innerText='';
     dis2Num='';
 })
+window.addEventListener('keydown',(e)=>{
+    if(
+        e.key==='0'||
+        e.key==='1'||
+        e.key==='2'||
+        e.key==='3'||
+        e.key==='4'||
+        e.key==='5'||
+        e.key==='6'||
+        e.key==='7'||
+        e.key==='8'||
+        e.key==='9'||
+        e.key==='.'
+    ){
+        clickbuttonE1(e.key);
+    }else if(
+        e.key==='+'||
+        e.key==='-'||
+        e.key==='/'||
+        e.key==='%'
+    ){
+        clickOperation(e.key);
+    }else if(e.key==="*"){
+        clickOperation('x');
+    }else if(e.key=='Enter'||e.key==="x"){
+        clickEqual();
+    }
+});
+function clickbuttonE1(key){
+    numbersE1.forEach(button=>{
+        if(button.innerText===key){
+            button.click();
+        }
+    })
+}
+function clickOperation(key){
+    operationE1.forEach(button=>{
+        if(button.innerText===key);{
+        button.click();
+    }
+    })
+}
+function clickEqual(){
+    equalE1.click();
+}
